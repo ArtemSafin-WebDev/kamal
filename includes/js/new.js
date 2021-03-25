1d97638547761093d273c9f58771f6b779b94c78
Artem Safin <artemsafin.webdev@gmail.com>
2021-03-25 03:13:08 +0300

    Страница трансляции

--- includes/js/new.js
+++ includes/js/new.js
@@ -52,5 +52,63 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 
+  function countdown() {
+    const elements = Array.from(document.querySelectorAll(".js-countdown"));
+
+    elements.forEach((element) => {
+      const dateString = element.getAttribute("data-countdown");
+
+      var countDownDate = new Date(dateString).getTime();
+
+      // Update the count down every 1 second
+      var x = setInterval(function () {
+        // Get today's date and time
+        var now = new Date().getTime();
+
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
+
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor(
+          (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        );
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Display the result in the element with id="demo"
+        element.innerHTML = `
+          <div class="my-translation__countdown-element">
+            <div class="my-translation__countdown-element-value">${days}</div>
+            <div class="my-translation__countdown-element-key">дней</div>
+          </div>
+          <span class="my-translation__countdown-divider"> : </span>
+          <div class="my-translation__countdown-element">
+            <div class="my-translation__countdown-element-value">${hours}</div>
+            <div class="my-translation__countdown-element-key">часов</div>
+          </div>
+          <span class="my-translation__countdown-divider"> : </span>
+          <div class="my-translation__countdown-element">
+            <div class="my-translation__countdown-element-value">${minutes}</div>
+            <div class="my-translation__countdown-element-key">минут</div>
+          </div>
+          <span class="my-translation__countdown-divider my-translation__countdown-divider--pale"> : </span>
+          <div class="my-translation__countdown-element my-translation__countdown-element--pale">
+            <div class="my-translation__countdown-element-value my-translation__countdown-element-value--pale">${seconds}</div>
+            <div class="my-translation__countdown-element-key">секунд</div>
+          </div>
+        `;
+       
+
+        // If the count down is finished, write some text
+        if (distance < 0) {
+          clearInterval(x);
+          element.innerHTML = "Трансляция готова к показу";
+        }
+      }, 1000);
+    });
+  }
+
   quantity();
+  countdown();
 });
